43ed54163f1f2026c31d80a01293076ad301c99e
pradeep thakur <pradeep.thakur@infosys.com>
2022-08-24 19:46:16 +0530

    log log

--- src/main/java/counter/app/controller/CounterController.java
+++ src/main/java/counter/app/controller/CounterController.java
@@ -20,7 +20,6 @@ public class CounterController {
     log.info("An INFO Message");
     log.warn("A WARN Message");
     log.error("An ERROR Message");
-    log.error("An ERROR Message");
     log.info("i am from slf4j2");
 
 
