0e6a554245af46186c651a455f8066bea39d7526
pradeep thakur <pradeep.thakur@infosys.com>
2022-10-11 14:07:37 +0530

    header method

--- src/main/java/counter/app/controller/CounterController.java
+++ src/main/java/counter/app/controller/CounterController.java
@@ -1,6 +1,9 @@
 package counter.app.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
@@ -58,6 +61,14 @@ public class CounterController {
     log.info("setting interval...{}", interval);
     CounterController.interval = interval;
     return "new interval set";
+  }
+
+
+
+  @GetMapping("/header")
+  public ResponseEntity<String> greeting(@RequestHeader("Authorization") String headervl) {
+    // code that uses the language variable
+    return new ResponseEntity<String>(headervl, HttpStatus.OK);
   }
 
 }
